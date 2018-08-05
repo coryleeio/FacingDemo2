89d7a6c53394759f36c4254ecbb416a9192080be
Cory Lee <cl10001123@gmail.com>
2018-08-04 22:54:33 -0400

    Add corpse refactor

---
+++ Assets/Scripts/UI/InputHint.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine.UI;
+
+namespace Gamepackage
+{
+    public class InputHint : UIComponent
+    {
+        private bool active = false;
+
+        public override void Hide()
+        {
+            active = false;
+            GetComponent<InputHint>().gameObject.SetActive(false);
+        }
+
+        public override void Show()
+        {
+            active = true;
+            GetComponent<InputHint>().gameObject.SetActive(true);
+        }
+
+        public void ShowText(string text)
+        {
+            GetComponent<Text>().text = text;
+            Show();
+        }
+
+        public override void Refresh()
+        {
+
+        }
+
+        public void Toggle()
+        {
+            if (active)
+            {
+                Hide();
+            }
+            else
+            {
+                Show();
+            }
+        }
+    }
+}
