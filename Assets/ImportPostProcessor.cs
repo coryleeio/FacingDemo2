0ec39f1a4d95c5c9419c4e25e73935dd83272061
Cory Lee <cl10001123@gmail.com>
2019-01-05 14:34:35 -0500

    Add sprites processor with our default settings for sprites folder

---
+++ Assets/ImportPostProcessor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Gamepackage
+{
+    public class ImportPostProcessor : AssetPostprocessor
+    {
+        void OnPreprocessTexture()
+        {
+            if (assetPath.Contains(".png"))
+            {
+                TextureImporter textureImporter = (TextureImporter)assetImporter;
+                if (assetPath.Contains("Sprites/"))
+                {
+                    // Spine needs this
+                    textureImporter.isReadable = true;
+
+                    textureImporter.textureType = TextureImporterType.Sprite;
+                    textureImporter.spriteImportMode = SpriteImportMode.Single;
+                    textureImporter.spritePixelsPerUnit = 128;
+
+                    TextureImporterSettings textureSettings = new TextureImporterSettings();
+                    textureImporter.ReadTextureSettings(textureSettings);
+                    textureSettings.spriteMeshType = SpriteMeshType.FullRect;
+                    textureImporter.SetTextureSettings(textureSettings);
+                }
+            }
+
+        }
+    }
+}
