fcda24ea37e0536cd679c4cac46e2e60123107d9
Cory Lee <cl10001123@gmail.com>
2019-09-10 20:42:59 -0400

    More skills

--- Assets/StreamingAssets/Mods/Core/Templates/Skills.sql
+++ Assets/StreamingAssets/Mods/Core/Templates/Skills.sql
@@ -9,6 +9,12 @@ CREATE TABLE IF NOT EXISTS "Skills" (
 );
 
 INSERT INTO "Skills" VALUES('SKILL_LONG_BLADES', 'skill.long.blades', 'Longsword', "1.0");
+INSERT INTO "Skills" VALUES('SKILL_SHORT_BLADES', 'skill.short.blades', 'Longsword', "1.0");
+INSERT INTO "Skills" VALUES('SKILL_MACES', 'skill.maces', 'Longsword', "1.0");
+INSERT INTO "Skills" VALUES('SKILL_STAVES', 'skill.staves', 'Longsword', "1.0");
+INSERT INTO "Skills" VALUES('SKILL_UNARMED', 'skill.unarmed', 'Longsword', "1.0");
 INSERT INTO "Skills" VALUES('SKILL_SPELLCASTING', 'skill.spellcasting', 'Longsword', "1.0");
-
+INSERT INTO "Skills" VALUES('SKILL_SHIELDS', 'skill.shields', 'Longsword', "1.0");
+INSERT INTO "Skills" VALUES('SKILL_ARCHERY', 'skill.archery', 'Longsword', "1.0");
+INSERT INTO "Skills" VALUES('SKILL_THROWING', 'skill.throwing', 'Longsword', "1.0");
 COMMIT;
