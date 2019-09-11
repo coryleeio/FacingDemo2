071cc67800c14a4abb9090e80c467d4e26021e72
Cory Lee <cl10001123@gmail.com>
2019-09-10 22:30:33 -0400

    Update

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -10,15 +10,11 @@ class Builder
         string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
         string[] levels = new string[] { "Assets/MainMenuScene.unity", "Assets/LoadingScene.unity", "Assets/GameScene.unity" };
 
-        var win32DirectoryPath = EnsureDirectoryExists(path + "/Win32");
         var win64DirectoryPath = EnsureDirectoryExists(path + "/Win64");
-        var linux32DirectoryPath = EnsureDirectoryExists(path + "/Linux32");
         var linux64DirectoryPath = EnsureDirectoryExists(path + "/Linux64");
-        var osxDirectoryPath = EnsureDirectoryExists(path + "/OSX");
+        var osxDirectoryPath = EnsureDirectoryExists(path + "/OSX64");
 
-        BuildPipeline.BuildPlayer(levels, win32DirectoryPath + "/Game.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
         BuildPipeline.BuildPlayer(levels, win64DirectoryPath + "/Game.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
-        BuildPipeline.BuildPlayer(levels, linux32DirectoryPath + "/Game.exe", BuildTarget.StandaloneLinux, BuildOptions.None);
         BuildPipeline.BuildPlayer(levels, linux64DirectoryPath + "/Game.exe", BuildTarget.StandaloneLinux64, BuildOptions.None);
         BuildPipeline.BuildPlayer(levels, osxDirectoryPath + "/Game.exe", BuildTarget.StandaloneOSX, BuildOptions.None);
     }
